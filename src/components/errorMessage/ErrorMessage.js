d75558824c81aaa7f7621549f8bbced9c25a88ee
rinat_k <rinat1kam@mail.ru>
2023-01-10 06:32:12 +0500

    add spinner & error handler

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,17 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+  const style = { 
+    display: 'block',
+    width: "250px",
+    height: "250px",
+    objectFit: 'contain',
+    margin: "0 auto",
+  }
+
+  return (
+    <img style={style} src={img} alt="error" />
+  );
+};
+
+export default ErrorMessage;
